0298587ee8b4d9fe9f14aea2c7d6dfa5c9eec520
Jeremy Yang <jeremyjyang@gmail.com>
2019-04-05 21:36:58 -0600

    IMPC.

--- R/impc_geno2pheno.R
+++ R/impc_geno2pheno.R
@@ -1,10 +1,10 @@
 library(readr)
 library(data.table)
 
-tcrd_geno2pheno_impc <- read_delim("data/geno2pheno_impc.tsv", "\t")
+tcrd_geno2pheno_impc <- read_delim("/home/data/TCRD/data/tcrd_geno2pheno_impc.tsv", "\t")
 setDT(tcrd_geno2pheno_impc)
 
-mpml_mouse_phen <- read_delim("/home/data/metap/mouse.phen.tsv.gz", "\t")
+mpml_mouse_phen <- read_delim("/home/data/metap/data/mouse.phen.tsv.gz", "\t")
 setDT(mpml_mouse_phen)
 
 n_mpml <- uniqueN(mpml_mouse_phen$human_protein_id)
@@ -18,3 +18,10 @@ writeLines(sprintf("Delta: %d (%.1f%%)", n_tcrd-n_mpml, 100*(n_tcrd-n_mpml)/n_mp
 # ftp://ftp.ebi.ac.uk/pub/databases/impc/release-9.2/csv/
 
 impc_g2p <- read_delim("/home/data/IMPC/data/release-9.2/IMPC_genotype_phenotype.csv", ",")
+
+ortholog <- read_delim("/home/data/TCRD/data/tcrd_ortholog_mouse.tsv", "\t",
+                       col_types="nccccccc")
+setDT(ortholog)
+ortholog[, c("id",  "taxid", "species", "mgi_url"):=NULL]
+
+impc_g2p <- merge(impc_g2p, ortholog, by.x="marker_accession_id", by.y="mgi_id", all.x=T, all.y=F)
