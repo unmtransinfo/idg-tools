7c8a28750913755fa4edea3227a9ec35a43e2b1c
Jeremy Yang <jeremyjyang@gmail.com>
2020-04-16 14:03:15 -0600

    Ok.

--- sql/target_list.sql
+++ sql/target_list.sql
@@ -11,14 +11,19 @@ SELECT DISTINCT
 	protein.uniprot AS "uniprotId",
 	protein.up_version AS "uniprotVersion",
 	protein.name AS "proteinName",
-	protein.stringid AS "ensemblProteinId",
 	protein.chr,
 	protein.description AS "proteinDesc",
-	protein.dtoid AS "dtoId"
+	protein.dtoid AS "dtoId",
+	protein.stringid AS "ensemblProteinId",
+	xref.value AS "ensemblGeneId"
 FROM
 	target
 JOIN
 	t2tc ON t2tc.target_id = target.id
 JOIN
 	protein ON protein.id = t2tc.protein_id
+JOIN
+	xref ON xref.protein_id = protein.id
+WHERE
+	xref.xtype = 'Ensembl' AND xref.value REGEXP '^ENSG'
         ;
