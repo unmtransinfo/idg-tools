7fb1692672d13c33feedc03a90ae9f5cac8632f4
Jeremy Yang <jeremyjyang@gmail.com>
2020-09-01 09:18:32 -0600

    Ok.

--- sql/gene_list.sql
+++ sql/gene_list.sql
@@ -1,7 +1,22 @@
 SELECT DISTINCT
-	sym
+	target.id AS "tcrdTargetId",
+	target.name AS "tcrdTargetName",
+	target.fam AS "tcrdTargetFamily",
+	target.tdl AS "TDL",
+	target.idg AS "idgList",
+	protein.sym AS "geneSymbol",
+	protein.geneid AS "ncbiGeneId",
+	xref.value AS "ensemblGeneId"
 FROM
-	protein
+	target
+JOIN
+	t2tc ON t2tc.target_id = target.id
+JOIN
+	protein ON protein.id = t2tc.protein_id
+JOIN
+	xref ON xref.protein_id = protein.id
+WHERE
+	xref.xtype = 'Ensembl' AND xref.value REGEXP '^ENSG'
 ORDER BY
-	sym
-	;
+	protein.sym
+        ;
