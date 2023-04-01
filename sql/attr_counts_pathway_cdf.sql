67c38f1249eea55930768a81550a69df9a5b8da0
Swastitp <123667826+Swastitp@users.noreply.github.com>
2023-03-31 20:38:41 -0600

    Update attr_counts_pathway_cdf.sql

--- sql/attr_counts_pathway_cdf.sql
+++ sql/attr_counts_pathway_cdf.sql
@@ -4,9 +4,9 @@
 --
 SELECT
 	p.sym AS "gene_sym",
-	COUNT(t2p.id) AS "attr_count",
+	COUNT(p.id) AS "attr_count",
 	(
-	  1.0 / (1.0 + exp(-1.702*((COUNT(t2p.id)-13.05)/24.56)))
+	  1.0 / (1.0 + exp(-1.702*((COUNT(p.id)-13.05)/24.56)))
 	) AS "attr_cdf"
 FROM
 	protein p
@@ -14,8 +14,8 @@ JOIN
 	t2tc ON p.id = t2tc.protein_id
 JOIN
 	target t ON t.id = t2tc.target_id
-JOIN
-	target2pathway t2p ON t.id = t2p.target_id
+--JOIN
+--	target2pathway t2p ON t.id = t2p.target_id
 GROUP BY
 	p.sym
 ORDER BY
