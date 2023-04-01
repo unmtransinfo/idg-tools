d709a9554fc38dcffef9b3dc4cad20b476de6adb
Swastitp <123667826+Swastitp@users.noreply.github.com>
2023-03-31 21:12:26 -0600

    Update ppi_counts.sql

--- sql/ppi_counts.sql
+++ sql/ppi_counts.sql
@@ -1,10 +1,10 @@
-SELECT
-        ppitype,
-        interaction_type,
-        evidence,
-        COUNT(id)
-FROM 
-        ppi
-GROUP BY 
-        ppitype, interaction_type, evidence
-;
+--SELECT
+--      ppitype,
+--        interaction_type,
+--        evidence,
+--        COUNT(id)
+--FROM 
+--       ppi
+--GROUP BY 
+--        ppitype, interaction_type, evidence
+--;
