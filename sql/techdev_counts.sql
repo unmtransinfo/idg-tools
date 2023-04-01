61fbb9337276c4a89c6430161af99a3321b5286c
Swastitp <123667826+Swastitp@users.noreply.github.com>
2023-03-31 21:16:52 -0600

    Update techdev_counts.sql

--- sql/techdev_counts.sql
+++ sql/techdev_counts.sql
@@ -1,14 +1,14 @@
 --
-SELECT
-	COUNT(tdi.protein_id) AS "protein_count",
-	tdc.id AS "techdev",
-	tdc.name AS "techdev_contact"
+--SELECT
+--	COUNT(tdi.protein_id) AS "protein_count",
+--	tdc.id AS "techdev",
+--	tdc.name AS "techdev_contact"
 	
-FROM
-	techdev_info tdi
-JOIN
-	techdev_contact tdc ON tdc.id = tdi.contact_id
-GROUP BY tdc.id
-ORDER BY tdc.id
-	;
+--FROM
+--	techdev_info tdi
+--JOIN
+--	techdev_contact tdc ON tdc.id = tdi.contact_id
+--GROUP BY tdc.id
+--ORDER BY tdc.id
+--	;
 --
